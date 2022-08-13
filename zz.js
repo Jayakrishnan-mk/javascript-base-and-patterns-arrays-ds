11163315c7b9522b6fd4927175ab829f9da0997f
Jayakrishnan <jayakrishnan800@gmail.com>
2022-08-13 12:33:05 +0530

    secconddd

--- zz.js
+++ zz.js
@@ -1,1 +1,19 @@
-sdfasdf
+const array = [0, 1, 2, 4, 5, 7];
+let count = 0;
+for (let i = 0; i < array.length; i++) {
+    for (var j = i + 1; j < array.length; j++) {
+        if (array[j] == (i + 1)) {
+            count++;
+            i++;
+        }
+        
+    }
+    console.log(' ');
+    if (count != 0) {
+        console.log(array[i], "->", array[j]);
+    }
+    else {
+
+        console.log(array[i]);
+    }
+}
