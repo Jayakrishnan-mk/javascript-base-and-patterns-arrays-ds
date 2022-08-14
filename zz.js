de1139bb392aa7d594264079de928166891fcf0e
Jayakrishnan <jayakrishnan800@gmail.com>
2022-08-14 13:31:14 +0530

    third

--- zz.js
+++ zz.js
@@ -1,5 +1,6 @@
 const array = [0, 1, 2, 4, 5, 7];
 let count = 0;
+const arr= [];
 for (let i = 0; i < array.length; i++) {
     for (var j = i + 1; j < array.length; j++) {
         if (array[j] == (i + 1)) {
@@ -8,12 +9,14 @@ for (let i = 0; i < array.length; i++) {
         }
         
     }
-    console.log(' ');
+    // console.log(' ');
     if (count != 0) {
-        console.log(array[i], "->", array[j]);
+        arr.push(array[i], "->", array[j]);
     }
     else {
 
-        console.log(array[i]);
+        arr.push(array[i]);
     }
 }
+console.log(arr);
+console.log('git pushed');
